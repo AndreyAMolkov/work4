f9fb20cee88f6a2cadf9bb840e779878f363d6b1
Andrey A Molkov <molkov_aa@mail.ru>
2017-12-12 21:23:31 +0300

    it's all

--- 1/LAB4/main1.c
+++ 1/LAB4/main1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 #define OUT 0
 #define IN 1
@@ -6,23 +7,26 @@
 #define SIZE 50
 int main()
 {
-	int i = 0,j=0;
+	int i = 0,j=0,countN=0;
 	int flagEnt = OUT;// flag end of entered lines
 	char ch;
-	char buf[SIZE][SIZE] = { ' ' };
+	char *p[SIZE];
+	char str[SIZE][SIZE] = { ' ' };
+	
 	printf("Enter few of line of string:\n");
 	
-	while (flagEnt != END)
+	while (countN<=0 && *(p[i]=fgets(str[i],SIZE,stdin))!='\0')
 	{
 		
-		;
-
-
-
+		if (*str[i] == '\n')
+			countN++;
+		else
+			countN = 0;
+		i++;
 	}
 
-
-
-
+	
+	lineSort(p, i);
+	printLines(p,i );
 	return 0;
 }
--- 1/LAB4/task1.c
+++ 1/LAB4/task1.c
@@ -1,23 +1,27 @@
-
-void lineSort(char *str[], int size)
+#include "task1.h"
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#define SIZE 50
+void compare(const void *a,const void *b)
 {
+	if (strlen(*(char **)a) < strlen(*(char **)b))
+	{
+		return 1;
+	}
+	else
+		return -1;
 }
-void printLines(const char *str[], int size)
+void lineSort(char *str[SIZE], int size)
 {
+	qsort(str, size,sizeof(str), compare);//base functi0n quick sort
 }
-/*Написать программу, которая позволяет пользователю ввести несколько
-строк с клавиатуры, а затем выводящую их в порядке возрастания длины строки.
-Пояснение
-
-Строки вводятся до появления пустой строки и записываются в двумерный символьный массив.
-Одновременно происходит создание и заполнение массива указателей на char.
-После окончания ввода программа сортирует укзатели в массиве и 
-выводит строки в соответствии с отсортированными указателями.
-Состав
-Программа должна состоять из функций :
--void lineSort(char *str[], int size) - функция, сортирующая массив указателей
-- void printLines(const char *str[], int size) - функция, печатающая строки в порядке массива str
-- main() - организация ввода строк в двумерный символьный массив
-Создаются три файла : task1.h, task1.c, main1.c.
-
-*/
+void printLines(const char *str[SIZE], int size)
+{
+	int i = 0;
+	while (i<size)
+	{
+		fputs(str[i++], stdout);
+	}
+	
+}
